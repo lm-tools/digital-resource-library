95a5541ba04ce5d75a7be773f234fe8283d4426c
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-06-26 15:26:24 +0100

    Add initial search page that always returns full dataset
    - initial resource list page, without searching for now
    - rename find controller to dashboard controller

---
+++ test/integration/search.spec.js
@@ -0,0 +1,38 @@
+const { searchPage, googleTagManagerHelper } = require('./support/integrationSpecHelper');
+const expect = require('chai').expect;
+const { describe, it, before } = require('mocha');
+const resourcesListModel = require('../../app/models/resources');
+
+describe('Search', () => {
+  before(() => searchPage.visit());
+
+  it('should contain valid google tag manager data', () =>
+    expect(googleTagManagerHelper.getUserVariable()).to.equal('set-me-in-controller')
+  );
+
+  it('should display page', () =>
+    expect(searchPage.header()).to.equal('Explore the resource library')
+  );
+
+  describe('list', () => {
+    before(() => {
+      this.resourceList = searchPage.getResources();
+    });
+
+    it('should display all resources', () =>
+      expect(this.resourceList.length).to.eql(resourcesListModel.length)
+    );
+
+    it('should display correct title for all resources', () =>
+      expect(this.resourceList.map(i => i.title)).to.eql(resourcesListModel.map(i => i.title))
+    );
+    it('should display correct summary for all resources', () =>
+      expect(this.resourceList.map(i => i.summary)).to.eql(resourcesListModel.map(i => i.summary))
+    );
+    it('should display correct categories for all resources', () =>
+      expect(this.resourceList.map(i => i.categories))
+        .to.eql(resourcesListModel.map(i => i.category))
+    );
+  });
+});
+
