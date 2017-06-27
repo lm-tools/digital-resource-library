8c6fd4216f0b5be568cf761712aed76ed63c525b
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-06-27 16:00:23 +0100

    Add search by keyword to the model

--- app/models/resourcesModel.js
+++ app/models/resourcesModel.js
@@ -1,5 +1,17 @@
-module.exports = function ({ data }) {
-  const findAll = () => data;
+const _ = require('lodash');
 
-  return { findAll };
+module.exports = function ({ data }) {
+  const searchableFields = ['title', 'url', 'summary', 'journalMessage', 'category', 'groups'];
+
+  const searchInSingleItem = (object, query) => {
+    const objWithOnlyTextFields = _.pick(object, searchableFields);
+    const foundFields = _.values(objWithOnlyTextFields)
+      .filter(it => it.toString().toLowerCase().includes(query.toLowerCase()));
+    return foundFields.length > 0;
+  };
+
+  const findAll = () => data;
+  const findByKeyword = (keyword) => _.values(data).filter(it => searchInSingleItem(it, keyword));
+
+  return { findAll, findByKeyword };
 };
