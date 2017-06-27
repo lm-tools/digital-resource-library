6034645d56f9ee74f9c71c19bfdcbd2bfad97e47
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-06-27 17:04:46 +0100

    Search by keyword field

--- app/controllers/search-controller.js
+++ app/controllers/search-controller.js
@@ -4,7 +4,13 @@ const { resourceModel } = require('../appContext');
 
 /* GET home page. */
 router.get('/search', (req, res) => {
-  res.render('search', { resourceList: resourceModel.findAll() });
+  const searchParam = req.query.search || '';
+  res.render('search',
+    {
+      resourceList: resourceModel.findByKeyword(searchParam),
+      search: searchParam
+    }
+  );
 });
 
 module.exports = router;
