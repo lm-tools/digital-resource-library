3388602e0f82d72d93a834127b3b9a62eeb3e620
williamlacy <williamlacy@ucds.email>
2017-06-29 14:28:36 +0100

    Add category content to dashboard page

--- test/common/page_objects/dashboard-page.js
+++ test/common/page_objects/dashboard-page.js
@@ -10,5 +10,16 @@ class DashboardPage {
   header() {
     return this.browser.text('[data-test="header"]');
   }
+
+  extractText(name, context) {
+    return this.browser.text(`[data-test="${name}"]`, context);
+  }
+
+  getCategories() {
+    return this.browser.queryAll('[data-test="category"]').map(context => ({
+      title: this.extractText('category-title', context),
+      summary: this.extractText('category-summary', context),
+    }));
+  }
 }
 module.exports = DashboardPage;
--- test/integration/dashboard.spec.js
+++ test/integration/dashboard.spec.js
@@ -1,16 +1,32 @@
 const { dashboardPage, googleTagManagerHelper } = require('./support/integrationSpecHelper');
 const expect = require('chai').expect;
 const { describe, it } = require('mocha');
+const categoryListModel = require('../../app/data/categories');
 
 describe('Dashboard', () => {
+  before(() => dashboardPage.visit());
   it('should contain valid google tag manager data', () =>
-    dashboardPage.visit()
-      .then(() => expect(googleTagManagerHelper.getUserVariable()).to.equal('set-me-in-controller'))
+    expect(googleTagManagerHelper.getUserVariable()).to.equal('set-me-in-controller')
   );
 
   it('should display page', () =>
-    dashboardPage.visit()
-      .then(() => expect(dashboardPage.header()).to.equal('Explore the resource library'))
+    expect(dashboardPage.header()).to.equal('Explore the resource library')
   );
-});
 
+  describe('list', () => {
+    before(() => {
+      this.categoryList = dashboardPage.getCategories();
+    });
+
+    it('should display all categories', () =>
+      expect(this.categoryList.length).to.eql(categoryListModel.length)
+    );
+
+    it('should display correct title for all categories', () =>
+      expect(this.categoryList.map(i => i.title)).to.eql(categoryListModel.map(i => i.title))
+    );
+    it('should display correct summary for all categories', () =>
+      expect(this.categoryList.map(i => i.summary)).to.eql(categoryListModel.map(i => i.summary))
+    );
+  });
+});
