75afc9edcca90d850f3524d62e21d0c3e8c41f00
jessstubenbord <ixd@jess.pro>
2017-08-07 12:06:19 +0100

    initial change to category headings

--- app/data/categories.js
+++ app/data/categories.js
@@ -1,29 +1,29 @@
 /* eslint-disable max-len */
 const categoryList = [
   {
-    title: 'CVs and interviews',
-    summary: 'Advice and tools to help claimants prepare their CVs and get ready for interview',
+    title: 'Preparing for work',
+    summary: 'Sites for claimants getting back into work: childcare support, volunteering, mentoring',
   },
   {
-    title: 'Looking for work',
-    summary: 'Job boards, agency websites and advice on searching online',
+    title: 'CVs, cover letters and interviews',
+    summary: '*Advice and tools to help claimants prepare their CVs and get ready for interview',
+  },
+  {
+    title: 'Searching for work',
+    summary: 'Job boards, agency websites and advice on searching online advice on searching online advice on searching online',
   },
   {
     title: 'Skills, training and qualifications',
     summary: 'Sites to help claimants improve their skills or work towards a qualification',
   },
   {
-    title: 'Young jobseekers',
-    summary: 'Resources for young jobseekers: apprenticeships, work experience, internships',
+    title: 'Health, mental health and wellbeing',
+    summary: '*Resources for young jobseekers: apprenticeships, work experience, internships',
   },
   {
-    title: 'Finding out about work',
-    summary: 'Useful sites for research: job descriptions, grants, sector overviews, regulations',
-  },
-  {
-    title: 'Preparing for work',
-    summary: 'Sites for claimants getting back into work: childcare support, volunteering, mentoring',
-  },
+    title: 'Other support',
+    summary: '*Useful sites for research: job descriptions, grants, sector overviews, regulations',
+  }
 ];
 
 module.exports = categoryList;
