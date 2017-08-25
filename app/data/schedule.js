8b86b6e301ed41b252b875f0fe38f47384e99e0b
Not My Monkeys <notmymonkeys3000@gmail.com>
2017-08-25 16:41:12 +0100

    Update schedule to 1PM

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -2,17 +2,17 @@ const moment = require('moment');
 
 const schedule = [
   {
-    start: moment('2017-08-24T12:00+01'),
-    end: moment('2017-08-25T11:59:59+01'),
+    start: moment('2017-08-24T13:00+01'),
+    end: moment('2017-08-25T12:59:59+01'),
     resource: 'featured03',
   }, {
-    start: moment('2017-08-25T12:00+01'),
-    end: moment('2017-08-29T11:59:59+01'),
+    start: moment('2017-08-25T13:00+01'),
+    end: moment('2017-08-29T12:59:59+01'),
     resource: 'featured05',
   },
   {
-    start: moment('2017-08-29T12:00+01'),
-    end: moment('2017-08-30T11:59:59+01'),
+    start: moment('2017-08-29T13:00+01'),
+    end: moment('2017-08-30T12:59:59+01'),
     resource: 'featured06',
   },
 ];
