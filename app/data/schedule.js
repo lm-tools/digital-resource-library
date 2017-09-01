e2713b30f5080d21365a95c983f9c882a1ec5ea3
slightlyamish <slightlyamish@gmail.com>
2017-09-01 11:32:37 +0100

    updated featured schedule

--- app/data/schedule.js
+++ app/data/schedule.js
@@ -2,17 +2,27 @@ const moment = require('moment');
 
 const schedule = [
   {
-    start: moment('2017-08-24T13:00+01'),
-    end: moment('2017-08-25T12:59:59+01'),
-    resource: 'featured03',
+    start: moment('2017-09-01T13:00+01'),
+    end: moment('2017-09-04T12:59:59+01'),
+    resource: 'featured02',
   }, {
-    start: moment('2017-08-25T13:00+01'),
-    end: moment('2017-08-29T12:59:59+01'),
+    start: moment('2017-09-04T13:00+01'),
+    end: moment('2017-09-05T12:59:59+01'),
+    resource: 'featured03',
+  },
+  {
+    start: moment('2017-09-05T13:00+01'),
+    end: moment('2017-09-06T12:59:59+01'),
+    resource: 'featured04',
+  },
+  {
+    start: moment('2017-09-06T13:00+01'),
+    end: moment('2017-09-07T12:59:59+01'),
     resource: 'featured05',
   },
   {
-    start: moment('2017-08-29T13:00+01'),
-    end: moment('2017-08-30T12:59:59+01'),
+    start: moment('2017-09-08T13:00+01'),
+    end: moment('2017-09-11T12:59:59+01'),
     resource: 'featured06',
   },
 ];
