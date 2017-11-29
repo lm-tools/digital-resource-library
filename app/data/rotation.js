73f577327f75beaea0c2ea55b602d79eb818993c
Chilliwilly <williamlacy2@gmail.com>
2017-11-29 16:32:52 +0000

    add resource rotator and new resources (#95)
    * add resource rotator and new resources
    
    * tests only pass on a monday

---
+++ app/data/rotation.js
@@ -0,0 +1,12 @@
+const resources = [
+  'featured01',
+  'featured02',
+  'featured03',
+  'featured04',
+  'featured05',
+  'featured06',
+];
+
+const startRotation = new Date('2017-11-27T00:00:00');
+
+module.exports = { resources, startRotation };
