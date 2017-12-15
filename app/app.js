4d5f709b01e377b77c94a6aea70abe80410c6785
Not My Monkeys <notmymonkeys3000@gmail.com>
2017-12-15 17:32:44 +0000

    Version static asset

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ const i18n = require('./middleware/i18n');
 const errorHandler = require('./middleware/error-handler');
 const healthCheckController = require('./controllers/health-check-controller');
 const helmet = require('helmet');
+const layoutAssets = require('./models/assets');
 const breadcrumbViewModel = require('./controllers/breadcrumb-view-model');
 
 const app = express();
@@ -41,6 +42,7 @@ app.use((req, res, next) => {
   // eslint-disable-next-line no-param-reassign
   Object.assign(res.locals, {
     assetPath,
+    layoutAssets: layoutAssets({ assetPath }),
     basePath,
     googleTagManagerId,
     partials: {
