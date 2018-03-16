6049c3d19b9221f0b574019c2e8ebcf82b0e80a7
Sean Walsh <sean.walsh@gmail.com>
2018-03-16 13:58:58 +0000

    Coming down to three categories (#103)
    Adding new category names to resources and modifying the categories file with new blurbs.
    
    	modified:   app/data/categories.js
    	modified:   app/data/resources.js

--- app/data/categories.js
+++ app/data/categories.js
@@ -1,28 +1,16 @@
 /* eslint-disable max-len */
 const categoryList = [
   {
-    title: 'Preparing for work',
-    summary: 'Getting ready for work: researching jobs, childcare support, volunteering, mentoring',
-  },
-  {
-    title: 'CVs, cover letters and interviews',
-    summary: 'Advice and tools for preparing CVs, writing cover letters and getting ready for interview',
-  },
-  {
-    title: 'Searching for work',
-    summary: 'Sites to help claimants look for work safely and successfully',
+    title: 'Preparing for and finding work',
+    summary: 'Researching jobs, jobs listings, safe searching, CVs and cover letters, interviews, childcare, volunteering',
   },
   {
     title: 'Skills, training and qualifications',
     summary: 'Improving skills, working towards a qualification and finding apprenticeships',
   },
   {
-    title: 'Health, mental health and wellbeing',
-    summary: 'Helping claimants with health issues find support and get suitable work',
-  },
-  {
-    title: 'Other support',
-    summary: 'Getting through the system: legal rights, finding grants, language help',
+    title: 'Support',
+    summary: 'Health, mental health, wellbeing, legal rights, grants and other kinds of help',
   },
 ];
 
