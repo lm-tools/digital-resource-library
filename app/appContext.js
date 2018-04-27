07843ddf4389178abecb85c0ac1057611fb8e64c
williamlacy <williamlacy@ucds.email>
2018-04-27 15:07:30 +0000

    refactor configuration out of modules and into app context

--- app/appContext.js
+++ app/appContext.js
@@ -1,4 +1,14 @@
-const resourceModel = require('./models/resources-model')('drl-resources');
+const { elasticSearch } = require('./config');
+const logger = require('./../logger');
+const resourcesData = require('./data/resources');
+const SearchService = require('./services/search-service');
+const searchService = new SearchService({
+  rawData: resourcesData,
+  host: elasticSearch.host,
+  index: elasticSearch.index,
+  esType: 'resource',
+});
+const ResourceModel = require('./models/resources-model');
 const categoriesData = require('./data/categories');
 const categoryModel = require('./models/categories-model')({ data: categoriesData });
 const featuredData = require('./data/featured');
@@ -7,4 +17,6 @@ const rotation = require('./data/rotation');
 const scheduler = require('./models/scheduler')({ schedule, rotation });
 const featuredModel = require('./models/featured-model')({ data: featuredData, scheduler });
 
-module.exports = { resourceModel, categoryModel, featuredModel };
+const resourceModel = new ResourceModel({ searchService });
+
+module.exports = { resourceModel, categoryModel, featuredModel, searchService, logger };
--- app/models/resources-model.js
+++ app/models/resources-model.js
@@ -1,18 +1,27 @@
-const SearchService = require('../services/search-service');
-const resourcesRaw = require('./../data/resources');
-
-const esType = 'resource';
 const resourceIdProperty = 'resourceId';
 
-module.exports = esIndex => {
-  const searchService = new SearchService(esIndex);
-  return {
-    findAll: () => searchService.findAll(),
-    findByKeyword: keyword => searchService.find(keyword),
-    findById: (id) => resourcesRaw.find(i => i.resourceId === id),
-    raw: resourcesRaw,
-    esIndex,
-    esType,
-    resourceIdProperty
-  };
-};
+class ResourcesModel {
+
+  constructor({ searchService }) {
+    this.searchService = searchService;
+    this.resourceIdProperty = resourceIdProperty;
+  }
+
+  findAll() {
+    return this.searchService.findAll();
+  }
+
+  findByKeyword(keyword) {
+    return this.searchService.find(keyword);
+  }
+
+  findById(id) {
+    return this.getRawData().find(i => i[this.resourceIdProperty] === id);
+  }
+
+  getRawData() {
+    return this.searchService.rawData;
+  }
+}
+
+module.exports = ResourcesModel;
