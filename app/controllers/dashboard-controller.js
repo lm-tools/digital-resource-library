398fdb66472481103468cf368c3d44d5177012fe
williamlacy <williamlacy@ucds.email>
2018-05-10 10:46:57 +0000

    strip featured resource

--- app/controllers/dashboard-controller.js
+++ app/controllers/dashboard-controller.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const { categoryModel, featuredModel } = require('../appContext');
+const { categoryModel } = require('../appContext');
 const router = new express.Router();
-const moment = require('moment');
 
 router.get('/', (req, res) => {
-  const scheduleDate = moment(req.query.scheduleFor).toDate();
   const categoryList = categoryModel.findAll();
   const categoryListRow1 = categoryList.slice(0, 3);
   const categoryListRow2 = categoryList.slice(3, 6);
-  const featured = featuredModel.getRotation(scheduleDate);
-  res.render('dashboard', { categoryListRow1, categoryListRow2, featured });
+  res.render('dashboard', { categoryListRow1, categoryListRow2 });
 });
 
 module.exports = router;
